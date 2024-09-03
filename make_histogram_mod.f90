74075ed2eca2784e7bb52607b900236ecae1bcf0
rafaeldlz <rafaeld@hawaii.edu>
2024-09-03 01:12:09 -1000

    Update make_histogram_mod.f90

--- make_histogram_mod.f90
+++ make_histogram_mod.f90
@@ -9,26 +9,32 @@ module make_histogram_mod
               real :: set_min, set_max, bin_width
               integer :: n_bin, histo_velocity(100), histo_velx(100), bin_index, i
 
-              ! Make histogram
+              ! Set range of velocities
               set_min = 800.0
               set_max = -800.0
+              
+              ! Set number of bins
               n_bin = 100
+              
+              ! Set bin width
               bin_width = (set_max - set_min)/n_bin
               histo_velocity = 0
               histo_velx = 0
 
-              ! histogram for x-component
+              ! Histogram for x-component
               do i = 1, 1000
                bin_index = floor((velocity(1,i)-set_min)/bin_width) + 1
                if (bin_index .le. n_bin) histo_velx(bin_index) = histo_velx(bin_index) + 1
               end do
 
-              ! histogram for Velocities
+              ! Histogram for velocities
               do i = 1, 1000
                bin_index = floor((velocity_sq(i)-set_min)/bin_width) + 1
                if (bin_index .le. n_bin) histo_velocity(bin_index) = histo_velocity(bin_index) + 1
               end do
 
+              ! For each bin, write the number, minimum, maximum, midpoint, number of velocities,
+              ! and number of velocity x-components
               do i = 1, n_bin
                write (14,fmt="(I6,2x,F8.2,2x,F8.2,2x,F8.2,2x,I6,2x,I6)") i,(set_min+(i-1)*bin_width),(set_min+i*bin_width), &
                 &(set_min+(i-0.5)*bin_width),histo_velocity(i),histo_velx(i)
