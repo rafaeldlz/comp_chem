9b253d7d0d42eec1b3902afb154777163278d47b
rafaeldlz <rafaeld@hawaii.edu>
2024-09-03 01:15:29 -1000

    Update get_rand_mod.f90

--- get_rand_mod.f90
+++ get_rand_mod.f90
@@ -7,9 +7,12 @@ module get_rand_mod
               integer :: n
               real ,intent(out) :: r
 
+              ! Assign a random value to n, and set seed to size n
               call random_seed(size=n)
               allocate(seed(n))
               call random_seed(get=seed)
+              
+              ! Assign a random value to r
               call random_number(r)
               deallocate(seed)
               return  
